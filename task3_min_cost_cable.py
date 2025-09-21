d4189df4cc512f6862c0efef32616c0fc94df15f
InnaKeba <kebainna@gmail.com>
2025-09-21 18:08:49 +0300

    Added solution for task 3 using heap-based cable merge algorithm

--- task3_min_cost_cable.py
+++ task3_min_cost_cable.py
@@ -1,1 +1,24 @@
 import heapq
+
+def smart_cable_combiner(cables):
+    if not cables:
+        return 0
+
+    heapq.heapify(cables) 
+    total_cost = 0
+
+    while len(cables) > 1:
+        
+        first = heapq.heappop(cables)
+        second = heapq.heappop(cables)
+
+        cost = first + second
+        total_cost += cost
+
+        heapq.heappush(cables, cost)
+
+    return total_cost
+# Приклад
+cables = [5, 3, 2, 4] # 2+3=5, 5+4=9, 5+9=14, всього 5+9+14= 28
+result = smart_cable_combiner(cables)
+print("Мінімальні витрати на з'єднання:", result)
